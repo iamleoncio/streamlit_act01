904efb65f4d6fe0deb29a9fa5936eb028f18c193
iamLeon <32927057+iamleoncio@users.noreply.github.com>
2025-02-19 16:33:06 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+
+st.title ("THIS IS THE **GREATEST** SHOW")
+st.subheader("It is always fun to be here!")
+st.write("Join us  and earn great salary !")
+st.divider()
+
+st.image("C:\CIADMIN\CARDMRIThematicalWallpaper_v1.png","This is a sample caption")
+
